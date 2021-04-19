49c2980806c1ed1e395ce9f7925ff81df6126a25
frauke <f.stehr@maastrichtuniversity.nl>
2021-04-19 12:14:29 +0200

    added js function to prevent submission if some sliders are still zero

--- impact_beliefs/static/js/sliders.js
+++ impact_beliefs/static/js/sliders.js
@@ -8,3 +8,38 @@ $('.slider').on('input change', function(){
         $(this).html(value);
      });
 })
+
+
+// for now define it based on element being zero; change later using event listener & set touched function
+function checkTouched() {
+              var sliders = document.getElementsByClassName("slider");
+              var num_touched = 0;
+
+                for (i = 0; i < sliders.length; i++) {
+                    if (sliders[i].value != 0) {
+                        num_touched = num_touched + 1;
+                    }
+                }
+                if (num_touched == sliders.length ) {
+                      form.submit();
+                }
+                else {
+                    event.preventDefault();
+                    document.getElementById('alert').style.display="block";
+                }
+            }
+
+
+            /*
+            function set_touched(event) {
+                var hidden_field_id = this.id.substring(0, this.id.length-7) + "touched";
+                var touch_element = document.getElementById(hidden_field_id);
+                touch_element.value = "True";
+                this.removeEventListener('change', set_touched)
+                }
+
+            var sliders = document.getElementsByClassName("slider");
+                for (i = 0; i < sliders.length; i++) {
+                    sliders[i].addEventListener('change', set_touched);
+                }
+             */
