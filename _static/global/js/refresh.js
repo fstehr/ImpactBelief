3257d783beeeeefce246c22a01703b592d965997
frauke <f.stehr@maastrichtuniversity.nl>
2021-04-26 19:52:12 +0200

    Call function in js refresh only on button click

--- _static/global/js/refresh.js
+++ _static/global/js/refresh.js
@@ -2,21 +2,30 @@
 
 // Here I tried to remove this variable
 function resetTabs() {
-    localStorage.removeItem('selectedTab');
+    console.log("Hello world!");
+
+    // This keeps active tab in front when refreshing page or when there are form errors
+    $('a[data-toggle="tab"]').click(function (e) {
+        e.preventDefault();
+        $(this).tab('show');
+    });
+
 }
 
-// This keeps active tab in front when refreshing page or when there are form errors
-$('a[data-toggle="tab"]').click(function (e) {
-    e.preventDefault();
-    $(this).tab('show');
-});
+    $('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
+        var id = $(e.target).attr("href");
+        localStorage.setItem('selectedTab', id)
+    });
 
-$('a[data-toggle="tab"]').on("shown.bs.tab", function (e) {
-    var id = $(e.target).attr("href");
-    localStorage.setItem('selectedTab', id)
-});
+    var selectedTab = localStorage.getItem('selectedTab');
+    if (selectedTab != null) {
+        $('a[data-toggle="tab"][href="' + selectedTab + '"]').tab('show');
+    }
 
-var selectedTab = localStorage.getItem('selectedTab');
-if (selectedTab != null) {
-    $('a[data-toggle="tab"][href="' + selectedTab + '"]').tab('show');
-}
+    window.onunload = (event) => {
+    console.log('The page is unloaded');
+};
+
+    window.onload = (event) => {
+    console.log('The page is loaded');
+};
