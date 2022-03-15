180df6b20d1a5850d288fc0f4d41a2fd30316f53
frauke <f.stehr@maastrichtuniversity.nl>
2022-03-15 18:47:40 +0100

    added img_id

--- pilot_2_projects/models.py
+++ pilot_2_projects/models.py
@@ -17,6 +17,7 @@ doc = """
 This is a pilot snippet testing whether it makes a difference (for beliefs & donations) 
 whether two projects are on one screen or not.
 This is the code with <b>2 projects per screen</b>. All currency variables are in US Dollars.
+Design is 2x2 (player.accuracy_bonus (within subj) x player.treatment (btw subj.))
 """
 
 
@@ -28,15 +29,20 @@ class Constants(BaseConstants):
     with open('pilot_2_projects/static/Parameters.csv', encoding='utf-8-sig') as parameters:
         paras = list(csv.DictReader(parameters, dialect='excel'))
 
-    num_rounds = len(paras)
+    # structure of experiment
+    num_parts = 2
+    num_rounds = len(paras) * num_parts
 
     # timing parameters for display
     duration_min = 15
-    feedback_bonus = 5
-    endowment = 40
     sec_intro = 3
     sec_per_matrix = 7
     sec_to_answer = 20
+
+    # incentives
+    endowment = 40
+    accuracy_bonus_low = 2
+    accuracy_bonus_high = 20
 
 
 class Subsession(BaseSubsession):
@@ -46,9 +52,22 @@ class Subsession(BaseSubsession):
             for p in self.session.get_participants():
                 paras = Constants.paras.copy()
 
-                # store shuffled list of parameters in participant vars, access each element by round number in pages.py
-                random.shuffle(paras)
-                p.vars['parameters'] = paras
+                # store shuffled list of img number & parameters in participant vars,
+                # access each element by round number in pages.py
+                p.vars['parameters'] = []
+                img = [*range(1, len(paras) + 1)]
+                p.vars['img_a'] = []   # initialize participant vars
+                p.vars['img_b'] = []
+
+                for i in range(1, Constants.num_parts + 1):
+                    a = random.sample(img, len(img))   # randomize which matrix is displayed for each project (matrix_80_i)
+                    b = random.sample(img, len(img))
+                    c = random.sample(paras, len(paras))
+                    p.vars['img_a'] += a
+                    p.vars['img_b'] += b
+                    p.vars['parameters'] += c
+                print('img_a is', p.vars['img_a'])
+                print('img_b is', p.vars['img_b'])
                 print(p.vars['parameters'])  # prints participant vars to double check randomization
 
                 # randomize on participant level whether project A is displayed on the left or right for all rounds
@@ -60,6 +79,36 @@ class Subsession(BaseSubsession):
                 # assign one payment round
                 p.vars['payment_round'] = random.choice(rounds)
                 # print("Payment round is", p.vars['payment_round'])
+
+                # randomly assign treatment order to participants
+                orders = ["NeutralMotivated", "MotivatedNeutral"]
+                p.vars['order'] = random.choice(orders)
+
+            # store some of the participant vars in the data set
+            for p in self.get_players():
+                # Define "part" variable in the beginning of the experiment
+                if self.round_number <= len(Constants.paras):
+                    p.part = 1
+                elif self.round_number <= Constants.num_rounds:
+                    p.part = 2
+
+                # Assign between subject treatment with pr(ExAnte) = 1/3, pr(ExPost) = 2/3
+                treatments = ["ExAnte", "ExPost", "ExPost"]
+                p.treatment = random.choice(treatments)
+
+                # Assign incentive strength to players in all rounds using treatment order assigned in first round
+                if p.participant.vars['order'] == "NeutralMotivated":
+                    p.order = "NeutralMotivated"
+                    if p.part == 1:
+                        p.accuracy_bonus = Constants.accuracy_bonus_high
+                    else:
+                        p.accuracy_bonus = Constants.accuracy_bonus_low
+                elif p.participant.vars['order'] == "MotivatedNeutral":
+                    p.order = "MotivatedNeutral"
+                    if p.part == 1:
+                        p.accuracy_bonus = Constants.accuracy_bonus_low
+                    else:
+                        p.accuracy_bonus = Constants.accuracy_bonus_high
 
 
 class Group(BaseGroup):
@@ -101,12 +150,20 @@ class Player(BasePlayer):
     trial_confidence_B = models.IntegerField(initial=-99, min=0, max=20)
     trial_donation_B = models.BooleanField(widget=widgets.RadioSelectHorizontal, choices=[[True, 'Yes'], [False, 'No']])
 
+    # treatment variables
+    order = models.StringField()
+    part = models.IntegerField()
+    accuracy_bonus = models.IntegerField()  # options are lo and hi, as defined in Constants
+    treatment = models.StringField()   # options as ExAnte and ExPost
+
+    comparison_id = models.IntegerField()
     comparison_type = models.StringField()
 
     # Characteristics of Project A
     num_x_A = models.IntegerField()
     price_A = models.IntegerField()
     efficiency_A = models.FloatField()
+    img_A = models.IntegerField()
 
     # Subject input of project A
     num_x_belief_A = models.IntegerField(blank=True, min=0, max=400, doc="records belief on number of Xs in matrix")
@@ -119,6 +176,7 @@ class Player(BasePlayer):
     num_x_B = models.IntegerField()
     price_B = models.IntegerField()
     efficiency_B = models.FloatField()
+    img_B = models.IntegerField()
 
     # Subject input of project B
     num_x_belief_B = models.IntegerField(blank=True, min=0, max=400, doc="records belief on number of Xs in matrix")
@@ -174,15 +232,30 @@ class Player(BasePlayer):
 # - test randomization of pictures!!! using console log with the picture names!
 
 # - emphasize that there is no (immediate) feedback on belief accuracy!
+# - maybe not call it estimation but best guess or belief or so in the instructions!
+# - as elicited by their donation in a dictator game with other participants at the end of the experiment
 
-# think about what to do with time-out variable
+# think about how to deal with with time-out variable
 
+# include honey pot
 
 # I saw once that the input field on the left was not de-activated --> de-bug!
 
-
+#  (as elicited by their donation in a dictator game with other participants at the end of the experiment) show a stronger inclination for motivated beliefs.
 
 ###### For experiment
 # - randomize treatments with high & low incentives
+
+# The experiment is programmed such that subjects:
+# -	who do not enter their belief estimate on time two times (within 20 seconds),
+# -	who do not manage to answer an attention check correctly,
+# -	who fail to answer the control questions testing their understanding of the instructions correctly within two attempts
+# are excluded from the experiment.
+
+
+# think about how to assign treatment most elegantly/practically
+# https://github.com/chkgk/otree_adaptive_treatment_assignment/blob/master/adaptive_assignment_demo/models.py
+
+
 
 
--- pilot_2_projects/pages.py
+++ pilot_2_projects/pages.py
@@ -64,44 +64,61 @@ class Donation(Page):
         # get current project from list of 'parameters'
         endowment = Constants.endowment
         project = self.participant.vars['parameters'][self.round_number - 1]
+        player.comparison_id = project['id']
         player.comparison_type = project['comparison']
         cheap_project_first = self.participant.vars['cheap_project_first'][self.round_number - 1]
-        path = "global/img/matrices/"
+        path = "global/img/matrices/matrix_"
 
         # use cheap_project_first variable to determine which project is displayed on the left
         if cheap_project_first == 1:
             # get characteristics of project A from parameters
             price_a = int(project['price_1'])
             player.num_x_A = int(project['num_X_1'])
-            image_a = path + project['image_title_1']
+            img_a_number = self.participant.vars['img_a'][self.round_number - 1]
+            image_a = path + str(player.num_x_A) + "_" + str(img_a_number) + ".png"
+            #print(image_a)
 
             # get characteristics of project B from parameters
             price_b = int(project['price_2'])
             player.num_x_B = int(project['num_X_2'])
-            image_b = path + project['image_title_2']
+            img_b_number = self.participant.vars['img_b'][self.round_number - 1]
+            image_b = path + str(player.num_x_B) + "_" + str(img_b_number) + ".png"
+            #print(image_b)
 
             # store parameters of project A in data set
             player.price_A = price_a
             player.efficiency_A = float(project['efficiency_1'])
+            player.img_A = img_a_number
 
             # store parameters of project B in data set
             player.price_B = price_b
             player.efficiency_B = float(project['efficiency_2'])
+            player.img_B = img_b_number
 
         else:
             # get characteristics of project A from parameters
             price_a = int(project['price_2'])
-            image_a = path + project['image_title_2']
-            player.price_A = price_a
             player.num_x_A = int(project['num_X_2'])
-            player.efficiency_A = float(project['efficiency_2'])
+            img_a_number = self.participant.vars['img_a'][self.round_number - 1]
+            image_a = path + str(player.num_x_A) + "_" + str(img_a_number) + ".png"
+            #print(image_a)
 
             # get characteristics of project B from parameters
             price_b = int(project['price_1'])
-            image_b = path + project['image_title_1']
-            player.price_B = price_b
             player.num_x_B = int(project['num_X_1'])
+            img_b_number = self.participant.vars['img_b'][self.round_number - 1]
+            image_b = path + str(player.num_x_B) + "_" + str(img_b_number) + ".png"
+            #print(image_b)
+
+            # store parameters of project A in data set
+            player.price_A = price_a
+            player.efficiency_A = float(project['efficiency_2'])
+            player.img_A = img_a_number
+
+            # store parameters of project B in data set
+            player.price_B = price_b
             player.efficiency_B = float(project['efficiency_1'])
+            player.img_B = img_b_number
 
         return {'endowment': endowment, 'img_a': image_a, 'img_b': image_b,
                 'price_a': price_a, 'price_b': price_b}
